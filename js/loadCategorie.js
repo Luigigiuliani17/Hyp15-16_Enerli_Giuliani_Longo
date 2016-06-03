2aad0a0a7340cbe9b437c80d9eb0f81910136b97
dami <damiano.enerli@mail.polimi.it>
2016-06-03 22:25:24 +0200

    completata pagina dinamica categorie

--- js/loadCategorie.js
+++ js/loadCategorie.js
@@ -53,10 +53,72 @@ function getFromDB(type){
         url: "http://tiim.altervista.org/php/getCategorie.php", //Relative or absolute path to file.php file
         data: {categorie_tipo:type},
         success: function(response) {
-            console.log(JSON.parse(response));
             // in categorie dovrebbe esserci un vettore di elementi categoria
-            var categorie=JSON.parse(response);
-
+            var categorie = JSON.parse(response);
+            // creo una variabile stringa per contenere il path:
+            var path = "";
+            // costruisco il path a seconda del tipo di categorie:
+            switch (parseInt(categorie[0].tipo)) {
+            	case 1:
+                	path = "";
+                	path += '<a  href="prodottiTutti.html">> Prodotti</a> > Categorie';
+                case 2:
+                	path = "";
+                	path += '<a  href="serviziTutti.html">> Smart Life</a> > Categorie';
+                case 3:
+                	path = "";
+                	path += '<a  href="assistenzaTutti.html">> Assistenza</a> > Categorie';
+            }
+            // creo una variabile stringa per contenere la somma delle singole descrizioni
+            var desc = "";
+            // creo una seconda variabile stringa per contenere il risultato contenitore delle immagini
+            var res = "";
+			for (var i = 0; i < categorie.length; i++) {
+            	// costruisco la descrizione
+                desc = desc + categorie[i].descrizione;
+                // costruisco il risultato a seconda del tipo della categoria che è stata selezionata
+                switch (parseInt(categorie[i].tipo)) {
+                case 1:
+                    res += '<div class="col-xs-12 col-sm-4 col-md-15 col-lg-15">';
+                    res += 	'<div>';
+                    res += 		'<section class="hsquare">';
+                    res += 			'<img src="/images/categorie/' + categorie[i].img + '" alt="Image not available, sorry." class="img-responsive">';
+                    res += 			'<p class="htxt">'+ categorie[i].nome + '</p>';
+                    res += 			'<p class="hbtn"><a href="prodottiPerCategoria.html?cat_id=' + categorie[i].cat_id + '" class="button buttonSB">Scopri</a></p>';
+                    res += 		'</section>';
+                    res += 	'</div>';
+                    res += '</div>';
+                    break;
+                case 2:
+                    res += '<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">';
+                    res += 	'<div>';
+                    res += 		'<section class="hsquare">';
+                    res += 			'<img src="/images/categorie/' + categorie[i].img + '" alt="Image not available, sorry." class="img-responsive">';
+                    res += 			'<p class="htxt">'+ categorie[i].nome + '</p>';
+                    res += 			'<p class="hbtn"><a href="serviziPerCategoria.html?cat_id=' + categorie[i].cat_id + '" class="button buttonSB">Scopri</a></p>';
+                    res += 		'</section>';
+                    res += 	'</div>';
+                    res += '</div>';
+                    break;
+                case 3:
+                    res += '<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">';
+                    res += 	'<div>';
+                    res += 		'<section class="hsquare">';
+                    res += 			'<img src="/images/categorie/' + categorie[i].img + '" alt="Image not available, sorry." class="img-responsive">';
+                    res += 			'<p class="htxt">'+ categorie[i].nome + '</p>';
+                    res += 			'<p class="hbtn"><a href="assistenzaPerCategoria.html?cat_id=' + categorie[i].cat_id + '" class="button buttonSB">Scopri</a></p>';
+                    res += 		'</section>';
+                    res += 	'</div>';
+                    res += '</div>';
+                    break;
+                default:
+                    res += "WTF";
+                    break;
+                }
+            }
+            $("#path").html(path);
+            $("#description").html(desc);
+            $("#categories").html(res);
         },
         error: function(request,error)
         {
